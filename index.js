04e29ef1259e09405c536168bf85395a5d588edc
zhangxinxin <1285367184@qq.com>
2024-01-30 23:23:52 +0800

    🎉feat: init project

---
+++ index.js
@@ -0,0 +1,5 @@
+import * as THREE from './build/three.module.js';
+import { mesh } from './model.js';
+
+mesh();
+console.log(THREE);
---
+++ model.js
@@ -0,0 +1,5 @@
+import * as THREE from './build/three.module.js';
+
+export const mesh = () => {
+  console.log(THREE, 'model');
+};
