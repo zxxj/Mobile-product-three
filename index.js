bc85858b61e0f67b598b5b05bff335b398eb00cd
zhangxinxin <1285367184@qq.com>
2024-02-03 03:10:01 +0800

    ✨feat: 获取模型中的某个节点与为模型设置PBR材质并设置纹理贴图

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { mesh } from './model.js';
 import { resize } from './resize.js';
+import { loadGLTF } from './model.js';
 
+// 场景
 const scene = new THREE.Scene();
 
-scene.add(mesh);
+// 加载模型
+loadGLTF('/public/', '手机.gltf');
 
+// 相机
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(100, 100, 100);
+camera.position.set(0, 0, 300);
 camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+// 环境光
+const ambientLgiht = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLgiht);
 
+// 平行光1
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(400, 200, 300);
+scene.add(directionalLight);
+
+// 平行光2
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight2.position.set(-400, -200, -300);
+scene.add(directionalLight2);
+
+// 渲染器
+const renderer = new THREE.WebGLRenderer({ antialias: true }); // 开启抗锯齿
+renderer.setSize(window.innerWidth, window.innerHeight); // 设置画布尺寸
+renderer.setPixelRatio(window.devicePixelRatio); // 设置设备像素比,防止canvas画布输出模糊
+
+// 渲染动画帧
 const render = () => {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
@@ -26,12 +46,17 @@ const render = () => {
 
 render();
 
+// 轨道控制器
 const control = new OrbitControls(camera, renderer.domElement);
 
+// 坐标轴辅助器
 const axesHelper = new THREE.AxesHelper(100);
 scene.add(axesHelper);
 
+// dom
 document.body.appendChild(renderer.domElement);
 
+// 自适应宽高
 resize();
-export { camera, renderer };
+
+export { scene, camera, renderer };
--- model.js
+++ model.js
@@ -1,11 +1,41 @@
 import * as THREE from './build/three.module.js';
+import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
+import { scene } from './index.js';
 
-const geometry = new THREE.BoxGeometry(50, 50, 50);
+// 纹理贴图加载器
+const textureLoader = new THREE.TextureLoader();
 
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
+// gltf加载器
+const loader = new GLTFLoader();
 
-const mesh = new THREE.Mesh(geometry, material);
+// 加载gltf方法
+const loadGLTF = (gltfPath, gltfName) => {
+  loader.load(gltfPath + gltfName, (gltf) => {
+    // 找到name为手机的节点
+    const mesh = gltf.scene.getObjectByName('手机');
 
-export { mesh };
+    // 修改为pbr材质
+    mesh.material = new THREE.MeshPhysicalMaterial({
+      metalness: 1.0, // 金属度
+      roughness: 1.0, // 粗糙度
+      map: textureLoader.load('/public/basecolor.png'), // 颜色贴图
+      metalnessMap: textureLoader.load('/public/metallic.png'), // 金属度贴图
+      roughnessMap: textureLoader.load('/public/roughness.png'), // 粗糙度贴图
+      normalMap: textureLoader.load('/public/normal.png'), // 法线贴图
+      alphaMap: textureLoader.load('/public/opacity.png'), // alpha贴图
+      transparent: true, // 如果使用了aplhaMap,要开启透明计算
+    });
+
+    // 设置问题贴图的朝向
+    mesh.material.map.flipY = false;
+    mesh.material.normalMap.flipY = false;
+    mesh.material.metalnessMap.flipY = false;
+    mesh.material.roughnessMap.flipY = false;
+    mesh.material.alphaMap.flipY = false;
+
+    // 将模型添加到场景中
+    scene.add(gltf.scene);
+  });
+};
+
+export { loadGLTF };
